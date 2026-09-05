0a5facc4e14fa02e062f659bfc4588d293a918bd
DJ Jackal <djjackal14@gmail.com>
2026-09-05 17:14:12 +0930

    Add FiveM resource manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'OTSA-TalkTo'
+author 'Off The Street Australia'
+description 'Private staff/player talk sessions using routing buckets and ACE permissions.'
+version '1.0.0'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
